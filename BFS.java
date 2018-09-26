763624185b12b505c25ea08cc8f90d7589081206
MichaelPaul-Software <43254207+MichaelPaul-Software@users.noreply.github.com>
2018-09-26 08:00:20 -0400

    Create BFS.java

---
+++ BFS.java
@@ -0,0 +1,33 @@
+package BreadthFirstSearch;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BFS {
+	
+	public void bfs(Vertex root) {
+		
+		Queue<Vertex> queue = new LinkedList<>();
+		
+		root.setVisited(true);
+		
+		queue.add(root);
+		
+		while(!queue.isEmpty()) {
+			
+			Vertex actualVertex = queue.remove();
+			
+			System.out.println(actualVertex + " ");
+			
+			for(Vertex v : actualVertex.getNeighborList() ) {
+				
+				if (!v.isVisited()) {
+					
+					v.setVisited(true);
+					queue.add(v);
+				}
+			}
+		}
+	}
+
+}
